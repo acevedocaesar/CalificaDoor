fd3f0a9dc55d7bc15a76056854e780072ed6d7cd
acevedocaesar <acevedocaesar@gmail.com>
2022-10-30 15:26:43 -0500

    Cambios para uso de Postgresql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("1098654","Norman Anderson","Rojas","normannd@gmail.com");
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("19875428","Maria Celina","Torres Serrano","ctorres@gmail.com");
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("9544688","Juan Carlos","Camargo Perez","jcamargo@gmail.com");
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("9654570","Diego Fernando","Rangel Pinto","drangel@gmail.com");
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("987665556","Pedro Andres","Moreno Oyola","pmoreno@gmail.com");
-INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES("1774555","Juan Carlos","Martinez Puyana","jmartinez@gmail.com");
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('1098654','Norman Anderson','Rojas','normannd@gmail.com');
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('19875428','Maria Celina','Torres Serrano','ctorres@gmail.com');
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('9544688','Juan Carlos','Camargo Perez','jcamargo@gmail.com');
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('9654570','Diego Fernando','Rangel Pinto','drangel@gmail.com');
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('987665556','Pedro Andres','Moreno Oyola','pmoreno@gmail.com');
+INSERT INTO estudiantes (documento,nombres,apellidos,email) VALUES('1774555','Juan Carlos','Martinez Puyana','jmartinez@gmail.com');
